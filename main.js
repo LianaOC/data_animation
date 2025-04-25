14fc9428e8ff6f4b584f4fc5260718872fe8446a
Liana O'Cleirigh <liana.ocleirigh@bentley.com>
2025-04-25 11:39:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ import { Application } from 'https://cdn.jsdelivr.net/npm/@splinetool/runtime@0.
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/Z2r7lh4b64Zj9kRm/scene.splinecode');
+
+app.load('https://prod.spline.design/Z2r7lh4b64Zj9kRm/scene.splinecode').then(() => {
+    console.log('Scene loaded successfully');
+}).catch((error) => {
+    console.error('Error loading scene:', error);
+});
